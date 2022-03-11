82139dd626e4a16e6fdcf76ed8d0b724d06a6f0d
USMortality.com <USMortality@protonmail.com>
2022-03-10 23:33:04 -0500

    update data, add update and update last query

--- query/world_covid_deaths.sql
+++ query/world_covid_deaths.sql
@@ -1,22 +1,34 @@
 SELECT
-    year,
-    -- MONTH,
+    '2020/21' AS flu_season,
     sum(new_deaths) AS deaths
 FROM
     (
         SELECT
-            cast(mid(date, 6, 2) AS integer) AS MONTH,
-            left(date, 4) AS year,
+            STR_TO_DATE(`date`, "%Y-%c-%d") AS `date`,
             new_deaths
         FROM
             owid.imp_world
         WHERE
-            location = "North America"
+            location = "World"
     ) a
 WHERE
-    MONTH >= 6
-    AND MONTH <= 12
-GROUP BY
-    year;
-
--- MONTH;
+    `date` >= STR_TO_DATE("2021-06-01", "%Y-%c-%d")
+    AND `date` < STR_TO_DATE("2022-03-09", "%Y-%c-%d")
+UNION
+ALL
+SELECT
+    '2021/22' AS flu_season,
+    sum(new_deaths) AS deaths
+FROM
+    (
+        SELECT
+            STR_TO_DATE(`date`, "%Y-%c-%d") AS `date`,
+            new_deaths
+        FROM
+            owid.imp_world
+        WHERE
+            location = "World"
+    ) a
+WHERE
+    `date` >= STR_TO_DATE("2020-06-01", "%Y-%c-%d")
+    AND `date` < STR_TO_DATE("2021-03-09", "%Y-%c-%d");
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.csv?raw=true -O data/world.csv
